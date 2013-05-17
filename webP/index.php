68754afb21aee15b1307476a22e558ed501faf9b
Ka Ho <p07iswin@hotmail.com>
2013-05-17 13:52:26 +0800

    WebP commit
    WebP commit

---
+++ webP/index.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<title>WebP Demo Page (webpjs)</title>
+	<script type="text/javascript" src="assets/js/webpjs-0.0.2.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+
+
+<div class="main_wrapper">
+	<div>
+		<h1>WebP Demo Page (webpjs)</h1>
+	</div>
+	<div class="webp_wrapper">		
+		<div class="webp_left"><div>JPG</div><img alt="JPG" title="JPG" src="assets/images/Koala.jpg"/></div>
+		<div class="webp_right"><div>WebP</div><img alt="WebP" title="WebP" src="assets/images/Koala.webp"/></div>
+	</div>
+</div>
+
+</body>
+</html>
